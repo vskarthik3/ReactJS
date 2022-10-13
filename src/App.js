8505d99d6150e08852505fe5d6e2a15f73eb95f7
vskarthik3 <karthikvuchuru2000@gmail.com>
2022-10-13 13:37:05 +0530

    okey

--- src/App.js
+++ src/App.js
@@ -1,25 +1,11 @@
-import logo from './logo.svg';
-import './App.css';
-
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+import React from 'react';
+import react from 'react'
+class App extends React.Component{
+  render(){
+    return <div>
+      <h3>Hii Very Good Afternoon</h3>
+      <h6>This is the first ReactJS</h6>
     </div>
-  );
+  }
 }
-
-export default App;
+export default App
