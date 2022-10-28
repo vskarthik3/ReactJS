9248b2ce49313de2cd07e14cacedc7129c07a774
karthik <111734558+vskarthik3@users.noreply.github.com>
2022-10-28 23:21:43 +0530

    Add files via upload

--- src/App.js
+++ src/App.js
@@ -1,13 +1,27 @@
- import Navbar from './Compo/navbar';
+import React from 'react';
+import Navbar from './Compo/navbar';
 import Main from './Compo/html';
 import Logindetails from './Compo/form handling';
- 
-function App(){
-    return <div>
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
+
+
+
+function App (){
+    return  <><div>
+        <Router>
         <Navbar />
+        <Routes>
+            <Route path='/home' element={<home />} />
+            <Route path="/about" element={<about />} />
+            <Route path="/contact" element={<contact />} />
+            <Route path="/services" element={<services />} />
+            <Route path="/rate-up" element={<rate-up />} />
+            <Route path="/login" element={<login />} />
+        </Routes>
+        </Router>
         <Main />
         <Logindetails />
-    </div>
+            </div></>
 
 }
 export default App
